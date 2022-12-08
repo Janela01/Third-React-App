fb66ebf614481083afb323aaa82693fc8689a82a
SabaJanelidze <sjane2021@agruni.edu.ge>
2022-12-08 18:23:02 +0400

    change img src with hope to fix bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@
 export default function Navbar(){
       return(
             <nav className="nav">
-                  <img className="one"src='/images/pngwing.com.png' alt='sport img'/>
-                  <img className="two" src='/images/—Pngtree—lower third for social media_6075372.png' alt='sport img'/>
+                  <img className="one"src='./images/pngwing.com.png' alt='sport img'/>
+                  <img className="two" src='./images/—Pngtree—lower third for social media_6075372.png' alt='sport img'/>
             </nav>
       )
 }
