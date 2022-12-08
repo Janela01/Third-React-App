27b0a53bd03adea2b2d6f3e2256c61ac3c68b7d2
SabaJanelidze <sjane2021@agruni.edu.ge>
2022-12-08 18:30:27 +0400

    change img src with hope to fix bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero(){
       return(
             <div className="hero">
-                  <img src='./images/sports-stars-in-lockdown.jpg' alt='sport img'/>
+                  <img src='Third-React-App/images/sports-stars-in-lockdown.jpg' alt='sport img'/>
                   <div className='wrapper'>
                         <h1>Sport News </h1>
                         <p>Sport | The latest results from sports events around the world: football, rugby, tennis, cycling ... Euronews keeps you in the loop.</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@
 export default function Navbar(){
       return(
             <nav className="nav">
-                  <img className="one"src='./images/pngwing.com.png' alt='sport img'/>
-                  <img className="two" src='./images/—Pngtree—lower third for social media_6075372.png' alt='sport img'/>
+                  <img className="one"src='Third-React-App/images/pngwing.com.png' alt='sport img'/>
+                  <img className="two" src='Third-React-App/images/—Pngtree—lower third for social media_6075372.png' alt='sport img'/>
             </nav>
       )
 }
